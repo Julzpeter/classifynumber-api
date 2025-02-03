b949a15fec7571e63bd240a9d184a98f5e4b7fca
Julzpeter <chepngetichjuliet@gmail.com>
2025-02-03 15:57:03 +0300

    allowed hosts in settings.py

--- number_classification_api/settings.py
+++ number_classification_api/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-m^(g9pnchuxjkt#$lf^33^b%q36g^q@16ajqb#l*#_q2k=s4wa
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['classifynumber-api.onrender.com','localhost','127.0.0.1']
 
 
 # Application definition
